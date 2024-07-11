4b49e977f91e9755f2755aa1cad96649011a6ab3
hyynee04 <huyblue2504@gmail.com>
2024-07-11 22:25:43 +0700

    React Component

---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    //JSX
+    render() {
+        return(
+            <div>My first Component</div>
+        )
+    }
+
+}
+
+export default MyComponent;
