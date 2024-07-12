ea4ae2488336233344fb9420019445d1d3eb4053
hyynee04 <huyblue2504@gmail.com>
2024-07-12 15:59:33 +0700

    form

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -21,13 +21,30 @@ class MyComponent extends React.Component {
         console.log(event.pageX)
     }
 
+    handleOnChange = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleOnSubmit = (event) => {
+        event.preventDefault()
+        console.log(this.state)
+    }
+
     //JSX
     render() {
-        return(
+        return (
             <div>
-                Name: {this.state.name} - Instagram: {this.state.ig} - Age: {this.state.age} <br/>
-                <button onClick={(event) => {this.handleClick(event)}}>Click me</button>
-                <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+                Name: {this.state.name} - Instagram: {this.state.ig} - Age: {this.state.age} <br />
+                <form onSubmit={(event) => this.handleOnSubmit(event)}>
+                    <input
+                        value={this.state.name}
+                        onChange={(event) => this.handleOnChange(event)}
+                        type="text"
+                    />
+                    <button>Submit</button>
+                </form>
             </div>
         )
     }
