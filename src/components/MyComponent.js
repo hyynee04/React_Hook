2967169a3fd85feebdda151910c21bab4c161436
hyynee04 <huyblue2504@gmail.com>
2024-07-12 15:41:02 +0700

    DOM events & setState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,11 +7,27 @@ class MyComponent extends React.Component {
         age: 27
     }
 
+    handleClick = (event) => {
+        console.log(`Name: `, this.state.name)
+        console.log(event)
+
+        this.setState({
+            name: 'Dylan',
+            age: Math.floor(Math.random() * 100)
+        })
+    }
+
+    handleOnMouseOver = (event) => {
+        console.log(event.pageX)
+    }
+
     //JSX
     render() {
         return(
             <div>
-                Name: {this.state.name} - Instagram: {this.state.ig} - Age: {this.state.age}
+                Name: {this.state.name} - Instagram: {this.state.ig} - Age: {this.state.age} <br/>
+                <button onClick={(event) => {this.handleClick(event)}}>Click me</button>
+                <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
             </div>
         )
     }
