ba7d5a25224daad7a611d62fe63606e83abb2352
hyynee04 <huyblue2504@gmail.com>
2024-07-14 21:25:23 +0700

    Setup boostrap & react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,50 +1,12 @@
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 
-import React from 'react';
-import MyComponent from './components/MyComponent';
+const App = () => {
 
-
-class App extends React.Component {
-  render() {
-    return(
-      <div className='app-container'>
-
-        <MyComponent></MyComponent>
-      </div>
-    )
-  }
+  return (
+    <div className="app-container">
+      HELLO WORLD!
+    </div>
+    );
 }
-
-
-// const App = () => {
-//   return(
-//     <div>
-//       Hello World!
-//       <MyComponent></MyComponent>
-//     </div>
-//   )
-// }
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           HELLO WORLD!
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button><br />
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//     );
-// }
 
 export default App;
