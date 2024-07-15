51d67c37259c373226625f67e112ef52200e8210
hyynee04 <huyblue2504@gmail.com>
2024-07-15 10:50:21 +0700

    Index routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,8 @@ const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="/">ReactJS</Navbar.Brand>
+                {/* <Navbar.Brand href="/">ReactJS</Navbar.Brand> */}
+                <NavLink to="/" className='navbar-brand'>ReactJS</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = (props) => {
+    return (
+        <div className="homepage-container">
+            Home Page component
+        </div>
+    )
+}
+
+export default HomePage;
